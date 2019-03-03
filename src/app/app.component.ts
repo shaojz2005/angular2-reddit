ac0290fe7fda27537b5b8cfde3cf599fa7047f0b
Aeven <shaojz2005@qq.com>
2019-03-03 16:26:59 +0800

    finish this demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular2-reddit';
+  articles: Article[];
+  constructor(){
+    this.articles = [
+      new Article('1','http://www.baidu.com/',3),
+      new Article('2','2',3),
+      new Article('3','2',3),
+    ];
+  }
+  addArticle(title:HTMLInputElement, link:HTMLInputElement):boolean{
+    console.log(`Adding article title:${title.value} and link:${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+  sortedArticles(): Article[]{
+    return this.articles.sort((a:Article, b:Article)=>b.votes - a.votes);
+  }
 }
